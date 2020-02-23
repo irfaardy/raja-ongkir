b6463b09a82c468ceb3f57a89b376bb01f4122c1
noob-programmer-x <61367599+noob-programmer-x@users.noreply.github.com>
2020-02-23 08:59:52 +0700

    Update rajaongkir.php

--- resources/config/irfa/rajaongkir.php
+++ resources/config/irfa/rajaongkir.php
@@ -1,9 +1,31 @@
 <?php 
 return [ 
+/*
+    |--------------------------------------------------------------------------
+    | End Point Api ( Server Configuration )
+    |--------------------------------------------------------------------------
+    |
+    | Starter : http://rajaongkir.com/api/starter
+    | Basic : http://rajaongkir.com/api/basic
+    | Pro : http://pro.rajaongkir.com/api
+    |
+    */
+     'account_type' => env('RAJAONGKIR_ACCOUNT_TYPE','starter'),
+/*
+    |--------------------------------------------------------------------------
+    | API key
+    |--------------------------------------------------------------------------
+    | You can get API key in www.rajaongkir.com
+    |
+    */
 
-		'account_type' => env('RAJAONGKIR_ACCOUNT_TYPE','starter'),
-
-		'api_key' => env('RAJAONGKIR_API_KEY','your-api-key'),
+     'api_key' => env('RAJAONGKIR_API_KEY','your-api-key'),
+/*
+    |--------------------------------------------------------------------------
+    | For Caching
+    |--------------------------------------------------------------------------
+    | You are free to change the cache configuration.
+    */
 
 		'province_table' => env('RAJAONGKIR_PROV_TABLE','ro_province'),
 
@@ -11,7 +33,6 @@ return [
 		
 		// Cache supported database,and file, default value : database 
 		'cache_type' => env('RAJAONGKIR_CACHE','database'),
-
 
 
 ];
