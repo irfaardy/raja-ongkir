4dcfb4ab21fc17fa9c7d22f86c3fd3fb625f6627
noob-programmer-x <61367599+noob-programmer-x@users.noreply.github.com>
2020-02-23 08:55:27 +0700

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,6 +1,22 @@
 <?php
 	$config = [
+/*
+    |--------------------------------------------------------------------------
+    | End Point Api ( Server Configuration )
+    |--------------------------------------------------------------------------
+    |
+    | Starter : http://rajaongkir.com/api/starter
+    | Basic : http://rajaongkir.com/api/basic
+    | Pro : http://pro.rajaongkir.com/api
+    |
+    */
 		'account_type' => 'starter',
-
+/*
+    |--------------------------------------------------------------------------
+    | API key
+    |--------------------------------------------------------------------------
+    | You can get API key in www.rajaongkir.com
+    |
+    */
 		'api_key' => 'your-api-key',
 	];
