42c748e6d6db89fce9f140dbbb27d7c775a89929
IRFA <irfardy2@gmail.com>
2020-03-17 13:29:46 +0700

    Raja ongkir setting

--- resources/config/irfa/rajaongkir.php
+++ resources/config/irfa/rajaongkir.php
@@ -6,9 +6,9 @@ return [
     | End Point Api ( Server Configuration )
     |--------------------------------------------------------------------------
     |
-    | Starter : http://rajaongkir.com/api/starter
-    | Basic : http://rajaongkir.com/api/basic
-    | Pro : http://pro.rajaongkir.com/api
+    | Starter   : http://rajaongkir.com/api/starter
+    | Basic     : http://rajaongkir.com/api/basic
+    | Pro       : http://pro.rajaongkir.com/api
     |
     */
     'account_type' => env('RAJAONGKIR_ACCOUNT_TYPE', 'starter'),
@@ -26,13 +26,17 @@ return [
     | For Caching
     |--------------------------------------------------------------------------
     | You are free to change the cache configuration.
+    | province_table for name table of province
+    | city_table for name table of city
+    | cache_type for type of cache
+    |
+    | Cache supported database,and file. If you can't use cache, set value to null
     */
 
     'province_table' => env('RAJAONGKIR_PROV_TABLE', 'ro_province'),
 
     'city_table' => env('RAJAONGKIR_CITY_TABLE', 'ro_city'),
 
-    // Cache supported database,and file, default value : database
-    'cache_type' => env('RAJAONGKIR_CACHE', 'file'),
+    'cache_type' => env('RAJAONGKIR_CACHE', null),
 
 ];
