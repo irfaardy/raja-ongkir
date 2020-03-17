c1f56a9758faaa5001fbdaf0414ff6e1e210367d
IRFA <irfardy2@gmail.com>
2020-03-17 04:21:54 +0000

    Apply fixes from StyleCI

--- src/Caching/CacheCurl.php
+++ src/Caching/CacheCurl.php
@@ -1,51 +1,59 @@
-<?php 
+<?php
+
 namespace Irfa\RajaOngkir\Caching;
 
-use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\Cache;
-use Irfa\RajaOngkir\Caching\DBImport;
 
 class CacheCurl extends DBImport
 {
-	private static $bucket;
-	private static $table;
-	private static $type;
+    private static $bucket;
+    private static $table;
+    private static $type;
 
-	protected static function caching($results) {
-		self::$bucket = $results;
-		return new static();
-	}
-	private static function _import() {
-		$cache_type = strtolower(config('irfa.rajaongkir.cache_type'));
-		if ($cache_type == 'file') {
-			Cache::store('file')->put('ro-cache-'.self::$table, self::$bucket);
-			echo"Cache has been created. ".self::formatBytes(strlen(serialize(Cache::get('ro-cache-'.self::$table))));
-		} elseif ($cache_type == 'database') {
-			self::import(self::$table, self::$bucket, self::$type);
-		} elseif ($cache_type == null) {
-			echo"Please set cache type.";
-		} else {
-			echo"Cache type is not supported.";
-		}
-		self::$bucket = null;
-		self::$type = null;
-		self::$table = null;
-	}
-	protected static function province() {
-		self::$table = config('irfa.rajaongkir.province_table');
-		self::$type = 'prov';
-		self::_import();
-	}
-	protected static function city() {
-		self::$table = config('irfa.rajaongkir.city_table');
-		self::$type = 'city';
-		self::_import();
-	}
-	private static function formatBytes($size, $precision = 2)
-	{
-		$base = log($size, 1024);
-		$suffixes = array('B', 'KB', 'MB', 'GB', 'TB');   
+    protected static function caching($results)
+    {
+        self::$bucket = $results;
 
-		return round(pow(1024, $base - floor($base)), $precision) .' '. $suffixes[floor($base)];
-	}
-	}
+        return new static();
+    }
+
+    private static function _import()
+    {
+        $cache_type = strtolower(config('irfa.rajaongkir.cache_type'));
+        if ($cache_type == 'file') {
+            Cache::store('file')->put('ro-cache-'.self::$table, self::$bucket);
+            echo'Cache has been created. '.self::formatBytes(strlen(serialize(Cache::get('ro-cache-'.self::$table))));
+        } elseif ($cache_type == 'database') {
+            self::import(self::$table, self::$bucket, self::$type);
+        } elseif ($cache_type == null) {
+            echo'Please set cache type.';
+        } else {
+            echo'Cache type is not supported.';
+        }
+        self::$bucket = null;
+        self::$type = null;
+        self::$table = null;
+    }
+
+    protected static function province()
+    {
+        self::$table = config('irfa.rajaongkir.province_table');
+        self::$type = 'prov';
+        self::_import();
+    }
+
+    protected static function city()
+    {
+        self::$table = config('irfa.rajaongkir.city_table');
+        self::$type = 'city';
+        self::_import();
+    }
+
+    private static function formatBytes($size, $precision = 2)
+    {
+        $base = log($size, 1024);
+        $suffixes = ['B', 'KB', 'MB', 'GB', 'TB'];
+
+        return round(pow(1024, $base - floor($base)), $precision).' '.$suffixes[floor($base)];
+    }
+}
--- src/Ongkir/Ongkir.php
+++ src/Ongkir/Ongkir.php
@@ -1,105 +1,125 @@
-<?php 
-/* 
-	Author: Irfa Ardiansyah <irfa.backend@protonmail.com>
+<?php
+
+/*
+    Author: Irfa Ardiansyah <irfa.backend@protonmail.com>
 */
+
 namespace Irfa\RajaOngkir\Ongkir;
 
-use Irfa\RajaOngkir\Ongkir\Func\Api;
-use Irfa\RajaOngkir\Caching\ROCache;
 use Exception;
+use Irfa\RajaOngkir\Caching\ROCache;
+use Irfa\RajaOngkir\Ongkir\Func\Api;
 
-class Ongkir extends Api {
+class Ongkir extends Api
+{
+    private static $arr;
+    private static $return;
 
-	 private static $arr;
-	 private static $return;
+    public static function find($arr)
+    {
+        if (is_array($arr)) {
+            self::$arr = $arr;
 
-	 public static function find($arr) {
-	 	if (is_array($arr)) {
-	 		self::$arr = $arr;
-	 	 	return new static();
-	 	 } else {
-	 	 	 throw new Exception("Parameter must be an array.");
-	 	 	 return false;
-	 	 }
-	       	
-		}
-	 public static function get() {
-	 	if (empty(self::$return)) {
-	 	  throw new Exception("Data is not defined.");
-	 	 	return false;
-	 	};
-	 	return self::$return;
-	 }
-	 public static function cachingProvince() {
-	 	self::cacheProvince();
-	 } 
-	 public static function cachingCity() {
-	 	self::cacheCity();
-	 }
+            return new static();
+        } else {
+            throw new Exception('Parameter must be an array.');
 
-	 public static function costDetails() {
-	 	self::$return = self::get_cost_details(self::$arr);
-	 	return new static();
-	 } 
+            return false;
+        }
+    }
 
-	 public static function courier() {
-	 	self::$return = self::get_courier(self::$arr);
-	 	return new static();
-	 }
+    public static function get()
+    {
+        if (empty(self::$return)) {
+            throw new Exception('Data is not defined.');
 
-	 public static function province() {
-	 	if (function_exists('config') AND function_exists('app')) {
-	 		$cache_type = strtolower(config('irfa.rajaongkir.cache_type'));
-	 		if ($cache_type == 'database') {
-	 			if (ROCache::checkProv()) {
-	 				if (count(ROCache::getProv(self::$arr)) > 0) {
-	 					$ret = ROCache::getProv(self::$arr);
-	 				} else {
-	 					$ret = self::get_province(self::$arr);
-	 				}
-	 			}
-	 		} elseif ($cache_type == 'file') {
-	 			$ret = ROCache::cacheFile(config('irfa.rajaongkir.province_table'), self::$arr);
-	 			if ($ret == null) {
-	 				throw new Exception("Cache is empty.");
-	 	 	 		return false;
-	 			}
+            return false;
+        }
 
-	 		} else {
-	 			$ret = self::get_province(self::$arr);
-	 		}
-	 	} else {
-	 			$ret = self::get_province(self::$arr);
-	 		}
-	 		self::$return = $ret;
-	 		return new static();
-	 	 	
-	 }
+        return self::$return;
+    }
 
-	 public static function city() {
-		if (function_exists('config') AND function_exists('app')) {
-	 		$cache_type = strtolower(config('irfa.rajaongkir.cache_type'));
-	 		if ($cache_type == 'database') {
-	 			if (ROCache::checkCity()) {
-	 				if (count(ROCache::getCity(self::$arr)) > 0) {
-	 					$ret = ROCache::getCity(self::$arr);
-	 				} else {
-	 					$ret = self::get_city(self::$arr);
-	 				}
-	 			}
-	 		} elseif ($cache_type == 'file') {
-	 			$ret = ROCache::cacheFile(config('irfa.rajaongkir.city_table'), self::$arr);
-	 			if ($ret == null) {
-	 				throw new Exception("Cache is empty. Try php artisan raja-ongkir:cache");
-	 	 	 		return false;
-	 			}
-	 		} else {
-	 			$ret = self::get_city(self::$arr);
-	 		}
-	 	} else {
-	 			$ret = self::get_city(self::$arr);
-	 		}
-	 	 	self::$return = $ret;
-	 	 	return new static();
-	 }
+    public static function cachingProvince()
+    {
+        self::cacheProvince();
+    }
+
+    public static function cachingCity()
+    {
+        self::cacheCity();
+    }
+
+    public static function costDetails()
+    {
+        self::$return = self::get_cost_details(self::$arr);
+
+        return new static();
+    }
+
+    public static function courier()
+    {
+        self::$return = self::get_courier(self::$arr);
+
+        return new static();
+    }
+
+    public static function province()
+    {
+        if (function_exists('config') and function_exists('app')) {
+            $cache_type = strtolower(config('irfa.rajaongkir.cache_type'));
+            if ($cache_type == 'database') {
+                if (ROCache::checkProv()) {
+                    if (count(ROCache::getProv(self::$arr)) > 0) {
+                        $ret = ROCache::getProv(self::$arr);
+                    } else {
+                        $ret = self::get_province(self::$arr);
+                    }
+                }
+            } elseif ($cache_type == 'file') {
+                $ret = ROCache::cacheFile(config('irfa.rajaongkir.province_table'), self::$arr);
+                if ($ret == null) {
+                    throw new Exception('Cache is empty.');
+
+                    return false;
+                }
+            } else {
+                $ret = self::get_province(self::$arr);
+            }
+        } else {
+            $ret = self::get_province(self::$arr);
+        }
+        self::$return = $ret;
+
+        return new static();
+    }
+
+    public static function city()
+    {
+        if (function_exists('config') and function_exists('app')) {
+            $cache_type = strtolower(config('irfa.rajaongkir.cache_type'));
+            if ($cache_type == 'database') {
+                if (ROCache::checkCity()) {
+                    if (count(ROCache::getCity(self::$arr)) > 0) {
+                        $ret = ROCache::getCity(self::$arr);
+                    } else {
+                        $ret = self::get_city(self::$arr);
+                    }
+                }
+            } elseif ($cache_type == 'file') {
+                $ret = ROCache::cacheFile(config('irfa.rajaongkir.city_table'), self::$arr);
+                if ($ret == null) {
+                    throw new Exception('Cache is empty. Try php artisan raja-ongkir:cache');
+
+                    return false;
+                }
+            } else {
+                $ret = self::get_city(self::$arr);
+            }
+        } else {
+            $ret = self::get_city(self::$arr);
+        }
+        self::$return = $ret;
+
+        return new static();
+    }
 }
