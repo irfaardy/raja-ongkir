5c8dc81e2244c892167b4061a3a0fbb9e831c3c4
IRFA <irfa.backend@protonmail.com>
2020-06-03 15:02:25 +0700

    Fix dropIfExist value

--- database/migrations/2020_02_21_103241_create_province.php
+++ database/migrations/2020_02_21_103241_create_province.php
@@ -27,6 +27,6 @@ class CreateProvince extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('province');
+        Schema::dropIfExists('ro_province');
     }
 }
