377493a1ae65be8c2633af9400eac715afd5553e
IRFA <irfardy2@gmail.com>
2020-06-17 21:00:54 +0700

    NEW Config

--- resources/config/irfa/rajaongkir.php
+++ resources/config/irfa/rajaongkir.php
@@ -23,6 +23,15 @@ return [
     'api_key' => env('RAJAONGKIR_API_KEY', 'your-api-key'),
     /*
     |--------------------------------------------------------------------------
+    | API Version (for account type basic and pro)
+    |--------------------------------------------------------------------------
+    | You can get API key in www.rajaongkir.com
+    |
+    */
+
+    'api_version' => env('RAJAONGKIR_API_VERSION', 'v2'),
+    /*
+    |--------------------------------------------------------------------------
     | For Caching
     |--------------------------------------------------------------------------
     | You are free to change the cache configuration.
@@ -32,11 +41,13 @@ return [
     |
     | Cache supported database,and file. If you can't use cache, set value to null
     */
-
+    
     'province_table' => env('RAJAONGKIR_PROV_TABLE', 'ro_province'),
 
     'city_table' => env('RAJAONGKIR_CITY_TABLE', 'ro_city'),
 
-    'cache_type' => env('RAJAONGKIR_CACHE', null),
+    'subdistrict_table' => env('RAJAONGKIR_SUBDISTRICT_TABLE', 'ro_subdistrict'),
+
+    'cache_type' => env('RAJAONGKIR_CACHE', 'database'),
 
 ];
